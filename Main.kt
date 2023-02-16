13a90e624b4633279d4c0c5e5e90cc477323a7aa
Tenno641 <120097440+Tenno641@users.noreply.github.com>
2023-02-16 09:45:41 +0200

    Stage 2

--- Main.kt
+++ Main.kt
@@ -1,11 +1,14 @@
+const val WATER = 200
+const val MILK = 50
+const val COFFEE = 15
+
 fun main() {
+    println("Write how many cups of coffee you will need:")
+    val cupsOfCoffee = readln().toInt()
     println("""
-        Starting to make a coffee
-        Grinding coffee beans
-        Boiling water
-        Mixing boiled water with crushed coffee beans
-        Pouring coffee into the cup
-        Pouring some milk into the cup
-        Coffee is ready!
-            """.trimIndent())
+        for $cupsOfCoffee of coffee you will need:
+        ${cupsOfCoffee * WATER} ml of water
+        ${cupsOfCoffee * MILK} ml of milk
+        ${cupsOfCoffee * COFFEE} g of coffee beans
+    """.trimIndent())
 }
