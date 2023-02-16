a7f550e8a51e073f427f5877ff6e3f2fc39bd7d2
Tenno641 <120097440+Tenno641@users.noreply.github.com>
2023-02-16 10:02:31 +0200

    Stage 3

--- Main.kt
+++ Main.kt
@@ -3,12 +3,21 @@ const val MILK = 50
 const val COFFEE = 15
 
 fun main() {
+    println("Write how many ml of water the coffee machine has:")
+    val water= readln().toInt()
+
+    println("Write how many ml of milk the coffee machine has:")
+    val milk = readln().toInt()
+
+    println("Write how many grams of coffee beans the coffee machine has:")
+    val coffee = readln().toInt()
+
     println("Write how many cups of coffee you will need:")
-    val cupsOfCoffee = readln().toInt()
-    println("""
-        for $cupsOfCoffee of coffee you will need:
-        ${cupsOfCoffee * WATER} ml of water
-        ${cupsOfCoffee * MILK} ml of milk
-        ${cupsOfCoffee * COFFEE} g of coffee beans
-    """.trimIndent())
+    val requiredCups = readln().toInt()
+
+    val possibleCups = minOf(water / WATER, milk / MILK, coffee / COFFEE)
+
+    if (requiredCups <= possibleCups) {
+        if (requiredCups == possibleCups) { print("Yes, I can make that amount of coffee") } else { print("Yes, I can make that amount of coffee (and even ${possibleCups - requiredCups} more than that)") }
+    } else { print("No, I can make only $possibleCups cups of coffee") }
 }
